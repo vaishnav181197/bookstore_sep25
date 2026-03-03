8ac74a48d43c5481f5d4dc2b947d5efdb83a8af2
vaish <vaish.qis@gmail.com>
2026-03-03 10:26:53 +0530

    'third'

--- src/users/pages/Career.jsx
+++ src/users/pages/Career.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Header from '../components/Header'
 import Footer from '../../components/Footer'
 import { FaLocationDot } from "react-icons/fa6";
 import { GrShare } from "react-icons/gr";
+import { IoClose } from "react-icons/io5";
+
 
 
 
 function Career() {
+
+  const [modalStatus,setModalStatus]=useState(false)
+
   return (
     <>
       <Header />
@@ -21,35 +26,73 @@ function Career() {
         <div className='w-full mt-10'>
           <h1 className="text-2xl">Current Openings</h1>
           <div className='flex justify-center my-5'>
-              <input type="text" className="border py-2" placeholder='Job Title'/>
-              <button className='bg-green-700 text-white p-2'>Search</button>
+            <input type="text" className="border py-2" placeholder='Job Title' />
+            <button className='bg-green-700 text-white p-2'>Search</button>
           </div>
           {/* Job List */}
           <div className='w-full'>
 
-              {/* Job Card */}
-              <div className='border-2 border-gray-500 shadow-lg py-3 px-2 flex flex-col md:grid grid-cols-7'>
-                  <div className='col-span-6'>
-                    <h1 className="text-lg mb-2">Job Title</h1>
-                    <hr />
-                    <p className='mt-5 flex gap-2 items-center'><FaLocationDot className='text-blue-800'/>  Location</p>
-                    <p className='mt-5'>Job Type :</p>
-                    <p className='mt-5'>Salary :</p>
-                    <p className='mt-5'>Qualification :</p>
-                    <p className='mt-5'>Experience :</p>
-                    <p className='mt-5'>Description :</p>
+            {/* Job Card */}
+            <div className='border-2 border-gray-500 shadow-lg py-3 px-2 flex flex-col md:grid grid-cols-7'>
+              <div className='col-span-6'>
+                <h1 className="text-lg mb-2">Job Title</h1>
+                <hr />
+                <p className='mt-5 flex gap-2 items-center'><FaLocationDot className='text-blue-800' />  Location</p>
+                <p className='mt-5'>Job Type :</p>
+                <p className='mt-5'>Salary :</p>
+                <p className='mt-5'>Qualification :</p>
+                <p className='mt-5'>Experience :</p>
+                <p className='mt-5'>Description :</p>
 
-                  </div>
-                  <div className='px-4'>
-                    <button className='bg-blue-800 text-light p-2 float-end md:p-4 text-white md:float-start rounded-sm flex gap-1 items-center font-semibold'>
-                      Apply
-                      <GrShare/>
-                    </button>
-                  </div>
               </div>
+              <div className='px-4'>
+                <button className='bg-blue-800 text-light p-2 float-end md:p-4 text-white md:float-start rounded-sm flex gap-1 items-center font-semibold'
+                onClick={()=>setModalStatus(true)}>
+                  Apply
+                  <GrShare />
+                </button>
+              </div>
+            </div>
 
 
           </div>
+
+          {
+            modalStatus &&
+            <div className='relative z-10' >
+              <div className='bg-gray-500/75 fixed inset-0'>
+                <div className='flex justify-center items-center min-h-screen'>
+                  <div style={{ minHeight: '400px', width: '500px' }} className='bg-white rounded-2xl'>
+                    <div className='bg-black text-white flex justify-between items-center p-3 rounded-t-2xl'>
+                      <h1 className='text-2xl'>Application Form</h1>
+                      <button onClick={() => setModalStatus(false)}>
+                        <IoClose />
+                      </button>
+                    </div>
+                    <div className='p-5'>
+                      <div className='grid grid-cols-2 gap-4 mb-5'>
+                        <input type="text" placeholder='Full Name' className='p-3 border border-gray-700 rounded-sm' />
+                        <input type="text" placeholder='Qualification' className='p-3 border border-gray-700 rounded-sm' />
+                        <input type="text" placeholder='Email ID' className='p-3 border border-gray-700 rounded-sm' />
+                        <input type="text" placeholder='Phone Number' className='p-3 border border-gray-700 rounded-sm' />
+                      </div>
+                      <textarea name="" placeholder='Cover Letter' className='p-2 border bg-white placeholder-gray-600 rounded-sm w-full mb-2' id=""></textarea>
+                      <div className='my-3'>
+                        <label htmlFor="" className='text-lg text-gray-600'>Resume</label>
+                        <input type="file" name="" id="" className='border w-full border-gray-600 rounded-sm file:bg-gray-400 file:p-3 file:text-white'/>
+                      </div>
+                    </div>
+                    <div className='bg-gray-200 p-3 flex justify-end gap-3 rounded-b-2xl'>
+                      <button className='p-2 border rounded-sm bg-red-500 text-white hover:bg-white hover:border-red-500 hover:text-red-500'>Reset</button>
+                      <button className='p-2 border rounded-sm bg-green-500 text-white hover:bg-white hover:border-green-500 hover:text-green-500'>Submit</button>
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+          }
+
 
         </div>
 
--- src/users/pages/UserProfile.jsx
+++ src/users/pages/UserProfile.jsx
@@ -1,8 +1,151 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import { FaRegEdit } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
+
+
+
+import Header from '../components/Header'
+import Footer from '../../components/Footer'
 
 function UserProfile() {
+
+  const [sellStatus, setSellStatus] = useState(true)
+  const [bookStatus, setBookStatus] = useState(false)
+  const [purchaseStatus, setPurchaseStatus] = useState(false)
+  const [modalStatus, setModalStatus] = useState(false)
+
   return (
-    <div>UserProfile</div>
+    <>
+      <Header />
+      <div className='min-h-[60vh]'>
+        <div className='w-full bg-gray-900 h-[40vh] relative'>
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOH2aZnIHWjMQj2lQUOWIL2f4Hljgab0ecZQ&s"
+            alt="profile_pic" className='rounded-full absolute left-5 -bottom-25' />
+        </div>
+        <div className='mt-30 px-5 md:px-20'>
+          <div className='flex justify-between '>
+            <h1 className="text-2xl">Username</h1>
+            <button className='text-blue-500 border border-blue-500 rounded-sm px-3 py-2 flex items-center gap-2 hover:bg-blue-500 hover:text-white'
+              onClick={() => setModalStatus(true)}>
+              Edit
+              <FaRegEdit />
+            </button>
+          </div>
+          <p className='text-justify my-3'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime corrupti ex omnis consectetur atque assumenda delectus earum excepturi asperiores, cum cupiditate, facere vel, eum dolores fuga minus in quae sequi?
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit non dolore nesciunt eos minima aut beatae? Deleniti perferendis iure provident temporibus, consectetur ullam laboriosam saepe optio modi. Nesciunt, similique explicabo.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati consequatur aperiam architecto tempore quis aliquam, tempora perspiciatis, magni minus non aliquid laboriosam assumenda vero vitae officiis temporibus molestiae officia veritatis!
+          </p>
+        </div>
+        <div className='flex justify-center items-center my-10'>
+          <div className={sellStatus ? 'p-3 border-gray-800 border-t border-l border-r rounded-t-sm text-blue-800' : 'p-3 border-b border-gray-800'}
+            onClick={() => { setSellStatus(true); setBookStatus(false); setPurchaseStatus(false) }} >
+            Sell Book
+          </div>
+          <div className={bookStatus ? 'p-3 border-gray-800 border-t border-l border-r rounded-t-sm text-blue-800' : 'p-3 border-b border-gray-800'}
+            onClick={() => { setSellStatus(false); setBookStatus(true); setPurchaseStatus(false) }}>
+            Book Status
+          </div>
+          <div className={purchaseStatus ? 'p-3 border-gray-800 border-t border-l border-r rounded-t-sm text-blue-800' : 'p-3 border-b border-gray-800'}
+            onClick={() => { setSellStatus(false); setBookStatus(false); setPurchaseStatus(true) }}>
+            Purchase History
+          </div>
+        </div>
+        {
+          sellStatus &&
+          <div className='px-5 md:px-50 mb-10'>
+            <div className='bg-gray-300 p-3'>
+              <h1 className="text-center my-5 text-xl">Book Details</h1>
+              <div className="md:grid grid-cols-2 gap-3">
+                <div>
+                  <input type="text" className="p-3 bg-white placeholder-gray-400 rounded-sm mb-3 w-full" placeholder='Title' />
+                  <input type="text" className="p-3 bg-white placeholder-gray-400 rounded-sm mb-3 w-full" placeholder='Author' />
+                  <input type="text" className="p-3 bg-white placeholder-gray-400 rounded-sm mb-3 w-full" placeholder='No Of Pages' />
+                  <input type="text" className="p-3 bg-white placeholder-gray-400 rounded-sm mb-3 w-full" placeholder='Image Url' />
+                  <input type="text" className="p-3 bg-white placeholder-gray-400 rounded-sm mb-3 w-full" placeholder='Price' />
+                  <input type="text" className="p-3 bg-white placeholder-gray-400 rounded-sm mb-3 w-full" placeholder='Discount Price' />
+                  <textarea name="" id="" className='w-full bg-white placeholder-gray-400 rounded-sm p-3' rows={'8'} placeholder='Abstract' >
+                  </textarea>
+                </div>
+                <div>
+                  <input type="text" className="p-3 bg-white placeholder-gray-400 rounded-sm mb-3 w-full" placeholder='Publisher' />
+                  <input type="text" className="p-3 bg-white placeholder-gray-400 rounded-sm mb-3 w-full" placeholder='Language' />
+                  <input type="text" className="p-3 bg-white placeholder-gray-400 rounded-sm mb-3 w-full" placeholder='ISBN' />
+                  <input type="text" className="p-3 bg-white placeholder-gray-400 rounded-sm mb-3 w-full" placeholder='Category' />
+                  <label htmlFor="imginp" className='flex justify-center'>
+                    <input type="file" className='hidden' id='imginp' />
+                    <img src="https://cdn.pixabay.com/photo/2017/02/07/02/16/cloud-2044823_1280.png" alt="fileinput"
+                      className='w-[80%] cursor-pointer' />
+                  </label>
+                </div>
+              </div>
+              <div className='p-2 flex justify-end gap-3'>
+                <button className='p-3 border bg-red-700 border-red-700 text-white rounded-sm hover:bg-white hover:text-red-700'>Reset</button>
+                <button className='p-3 border bg-green-700 border-green-700 text-white rounded-sm hover:bg-white hover:text-green-700'>Submit</button>
+              </div>
+            </div>
+          </div>
+        }
+        {
+          bookStatus &&
+          <div className='px-5 md:px-50 mb-10 shadow-lg border border-gray-100 p-4 flex flex-col justify-center items-center'>
+            <img src="https://i.pinimg.com/originals/b4/13/34/b41334a036d6796c281a6e5cbb36e4b5.gif" className='' width={'300px'} alt="no_books" />
+            <h1 className="text-2xl text-red-500">No Books Added Yet</h1>
+          </div>
+        }
+        {
+          purchaseStatus &&
+          <div className='px-5 md:px-50 mb-10 shadow-lg border border-gray-100 p-4 flex flex-col justify-center items-center'>
+            <img src="https://i.pinimg.com/originals/b4/13/34/b41334a036d6796c281a6e5cbb36e4b5.gif" className='' width={'300px'} alt="no_books" />
+            <h1 className="text-2xl text-red-500">No Books Purchased Yet</h1>
+          </div>
+        }
+
+        {/* Modal For Profile Edit */}
+        {
+          modalStatus &&
+          <div className='relative z-10' >
+            <div className='bg-gray-500/75 fixed inset-0'>
+              <div className='flex justify-start items-center min-h-screen'>
+                <div style={{ minHeight: '100vh', width: '500px' }} className='bg-white rounded-2xl flex flex-col justify-between'>
+                  <div className='bg-black text-white flex justify-between items-center p-3 rounded-t-2xl'>
+                    <h1 className='text-2xl'>Edit Profile</h1>
+                    <button onClick={() => setModalStatus(false)}>
+                      <IoClose />
+                    </button>
+                  </div>
+                  <div className='p-5'>
+                    <label htmlFor="profile_pic" className='flex justify-center relative'>
+                      <input type="file" name="" className='hidden' id="profile_pic" />
+                      <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt=""
+                        className='' width={'200px'} />
+                      <button className='p-2 bg-yellow-700 text-white absolute rounded bottom-5 right-35'>
+                        <FaRegEdit />
+                      </button>
+                    </label>
+                    <div className='py-5 flex flex-col gap-2'>
+                      <input type="text" placeholder='UserName' className='p-3 border border-gray-700 rounded-sm w-full' />
+                      <input type="text" placeholder='Password' className='p-3 border border-gray-700 rounded-sm w-full' />
+                      <input type="text" placeholder='Confirm Password' className='p-3 border border-gray-700 rounded-sm w-full' />
+                      <textarea name="" placeholder='Bio' className='p-2 border bg-white placeholder-gray-600 rounded-sm w-full mb-2' id=""></textarea>
+
+                    </div>
+                    
+                  </div>
+                  <div className='bg-gray-200 p-3 flex justify-end gap-3 rounded-b-2xl'>
+                    <button className='p-2 border rounded-sm bg-red-500 text-white hover:bg-white hover:border-red-500 hover:text-red-500'>Reset</button>
+                    <button className='p-2 border rounded-sm bg-green-500 text-white hover:bg-white hover:border-green-500 hover:text-green-500'>Submit</button>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </div>
+        }
+      </div>
+      <Footer />
+    </>
   )
 }
 
