fd6bda805934dcb5bd5e24cf64b3ab414ac47ba3
vaish <vaish.qis@gmail.com>
2026-08-28 16:25:54 +0530

    'newbaseurl'

--- src/services/base_url.js
+++ src/services/base_url.js
@@ -1,4 +1,4 @@
-const base_url="http://localhost:3000"
-// const base_url="https://bookstore-server-sep25.onrender.com"
+// const base_url="http://localhost:3000"
+const base_url="https://bookstore-server-sep25.onrender.com"
 
 export default base_url
